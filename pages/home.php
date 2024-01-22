7b1783b932db2ba900f70c34413c0c78cd69fe62
Kyle J. Harms <kyle.harms@cornell.edu>
2024-01-22 07:08:02 -0500

    activity materials

---
+++ pages/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Home</title>
+</head>
+
+<body>
+  <h1>INFO 2300</h1>
+  <p>This website is rendered server-side in PHP.</p>
+  <p>You're running PHP version: <strong><?php echo htmlspecialchars(phpversion()); ?></strong>.</p>
+</body>
+
+</html>
